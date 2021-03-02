72cc3716a9208d9e888f0b2a2638f351f2e298dc
mattomolewa <matthew.omolewa@gmail.com>
2021-03-02 15:34:07 +0100

    ...

---
+++ client/build/precache-manifest.5af8a90612e7663cbd2fd7cebee23b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f3d94e1a5e26a2f6f9d879cc68cc00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfc95233c90738816642",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5301757e4070c3c2d0d3",
+    "url": "/static/css/main.3d30460a.chunk.css"
+  },
+  {
+    "revision": "dfc95233c90738816642",
+    "url": "/static/js/2.20ba1742.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.20ba1742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5301757e4070c3c2d0d3",
+    "url": "/static/js/main.df451465.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
