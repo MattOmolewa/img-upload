fd0d05ed7bff62a0d115a88c3b3bd44114a2fc5e
mattomolewa <matthew.omolewa@gmail.com>
2021-03-02 15:32:31 +0100

    ...

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const PORT = 5000;
-require('dotenv').config();
+require("dotenv").config();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 const s3Client = new AWS.S3({
   accessKeyId: process.env.accessKeyId,
   secretAccessKey: process.env.secretAccessKey,
@@ -10,40 +15,43 @@ const s3Client = new AWS.S3({
 });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const multer = require('multer');
+const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-app.post('/upload-image', upload.array('files'), (req, res) => {
+app.post("/upload-image", upload.array("files"), (req, res) => {
   const promise_array = [];
-  req.files.forEach((file) => {
+  req.files.forEach(file => {
     const params = {
-      Acl: 'public-read',
+      Acl: "public-read",
       Bucket: process.env.Bucket,
-      Key: '' + file.originalname,
+      Key: "" + file.originalname,
       Body: file.buffer,
     };
     const putObjectPromise = s3Client.upload(params).promise();
     promise_array.push(putObjectPromise);
   });
   Promise.all(promise_array)
-    .then((values) => {
+    .then(values => {
       console.log(values);
-      const urls = values.map((value) => value.Location);
+      const urls = values.map(value => value.Location);
       console.log(urls);
       res.send(urls);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.status(500).send(err);
     });
 });
-app.get('/get-image', (req, res) => {
+app.get("/get-image", (req, res) => {
   // console.log(req.query);
   const query = req.query;
   s3Client.getObject(query, (err, data) => {
-    if (err) return res.status(400).send('Failed to get object');
+    if (err) return res.status(400).send("Failed to get object");
     res.send(data);
   });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => {
